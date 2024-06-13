c6600de489d40866963350d965d5c1aa2c13bc25
jeffreysaeteros <js11692@nyu.edu>
2024-06-13 00:46:48 -0400

    workpage

---
+++ app/work/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function WorkPage() {
+    return (
+        <section>
+            <h1 className="font-medium text-2xl mb-8 tracking-tighter">My Work</h1>
+            <p className="prose prose-neutral dark:prose-invert">
+                This is where your work experience goes.
+            </p>
+        </section>
+    )
+}
