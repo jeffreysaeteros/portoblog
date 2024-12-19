6cc111d66173acf1163c562c9d4c4b29274f1544
jeffreysaeteros <js11692@nyu.edu>
2024-12-19 14:34:03 -0500

    refac: navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,10 @@
 import Link from 'next/link'
 import React from "react";
+import Image from 'next/image';
 
 const navItems = {
-    '/': {
-        name: 'home',
+    '/about': {
+        name: 'about',
     },
     '/work': {
         name: 'work',
@@ -13,50 +14,15 @@ const navItems = {
     },
 }
 
-export function NameTransition() {
-    return (
-        <h1 className="transition-element text-3xl">
-            <span className="sr-only">jeffrey saeteros</span>
-            <span aria-hidden="true" className="block overflow-hidden  group relative">
-                {/* 'jeffrey saeteros' remains as white */}
-                <span className="inline-block transition-all duration-300 ease-in-out group-hover:-translate-y-full">
-                    {'jeffrey saeteros'.split('').map((letter, index) => (
-                        <span
-                            key={index}
-                            className="inline-block text-red-50"
-                            style={{ transitionDelay: `${index * 25}ms` }}
-                        >
-                            {letter === ' ' ? '\u00A0' : letter}
-                        </span>
-                    ))}
-                </span>
-                {/* 'jeffsaet' will be black when hidden and transition to white when revealed */}
-                <span className="inline-block absolute left-0 top-0 transition-all duration-300 ease-in-out translate-y-full group-hover:translate-y-0">
-                    {'jeffsaet'.split('').map((letter, index) => (
-                        <span
-                            key={index}
-                            className="inline-block transition-colors duration-300 ease-in-out"
-                            style={{
-                                transitionDelay: `${index * 25}ms`,
-                            }}
-                        >
-                            {letter}
-                        </span>
-                    ))}
-                </span>
-            </span>
-        </h1>
-    );
-}
-
-
-
 export const Navbar = () => {
     return (
-        <div className="flex flex-col px-8">
-            {/* <div className="w-40 shadow-md"> */}
-            <NameTransition />
-            {/* </div> */}
+        <div className=" flex flex-col mr-8">
+            <div className="text-4xl">
+                <Link href="/">
+                    jeffrey saeteros
+
+                </Link>
+            </div>
             <div className="flex flex-col gap-8 my-5">
                 {Object.entries(navItems).map(([path, { name }]) => {
                     return (
@@ -67,9 +33,9 @@ export const Navbar = () => {
                         >
                             {name}
                         </Link>
-                    )
+                    );
                 })}
             </div>
         </div>
-    )
-}
+    );
+};
