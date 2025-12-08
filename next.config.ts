8ee0e191a60f8134ef35daaba545d38bf7af07c6
jeffreysaeteros <js11692@nyu.edu>
2025-12-08 17:54:02 -0500

    reconfigging mdx custom components for futura font

--- next.config.ts
+++ next.config.ts
@@ -1,36 +1,15 @@
-import type { NextConfig } from 'next';
-import createMDX from '@next/mdx';
-import postgres from 'postgres';
+import createMDX from '@next/mdx'
 
-export const sql = postgres(process.env.POSTGRES_URL!, {
-    ssl: 'allow',
-});
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // Configure `pageExtensions` to include markdown and MDX files
+    pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+    // Optionally, add any other Next.js config below
+}
 
-const nextConfig: NextConfig = {
-    pageExtensions: ['mdx', 'ts', 'tsx'],
-    async redirects() {
-        if (!process.env.POSTGRES_URL) {
-            return [];
-        }
+const withMDX = createMDX({
+    extension: /\.(md|mdx)$/,
+})
 
-        let redirects = await sql`
-      SELECT source, destination, permanent
-      FROM redirects;
-    `;
-
-        return redirects.map(({ source, destination, permanent }) => ({
-            source,
-            destination,
-            permanent: !!permanent,
-        }));
-    },
-    // Note: Using the Rust compiler means we cannot use
-    // rehype or remark plugins. For my app, this is fine.
-    experimental: {
-        mdxRs: true,
-    },
-};
-
-const withMDX = createMDX({});
-
-export default withMDX(nextConfig);
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig)
